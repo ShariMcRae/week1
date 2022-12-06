18c1e1431a08f3cd9a1824b68273889ebe6442ac
Shari McRae <sharimcrae@protonmail.com>
2022-12-05 21:33:00 -0600

    added error handling in calculator.js

--- calculator.js
+++ calculator.js
@@ -3,20 +3,6 @@ class Calculator {
         console.log("construct Calculator");
         this.total = 0;
     }
-    validArguments(a, operator, b) {
-        console.log("validArguments(a, operator, b)")
-        const operators = ["+", "-", "*", "/"];
-        
-        if (isNaN(parseInt(a)))
-            alert(a + " is not a number.");
-        else if (isNaN(parseInt(b)))
-            alert(b + " is not a number.");
-        else if (!operators.includes(operator))
-            alert(operator + " is not a valid operator.");
-        else
-            return true;
-        return false;
-    }
     // add 
     addNumbers(a, b) {
         console.log("addNumbers(a, b)");
@@ -39,33 +25,87 @@ class Calculator {
     }
 }
 
-class App{
+class App {
     constructor() {
         this.calculator = new Calculator();
     }
-    promptForInput() {
-        
-        // store the first number
-        this.firstPrompt = prompt("Please enter first number.");
-        this.firstNumber = parseInt(this.firstPrompt);
-        console.log("firstNumber", this.firstNumber);
+    // prompt for and validate first number
+    hasValidFirstNumber() {
+
+        console.log("hasValidFirstNumber()");
+        let number = this.getValidNumber("Please enter first number.");
+        if (number !== null) {
+            this.firstNumber = number;
+            return true;
+        } else
+            return false;
+    }
+    // prompt for and validate second number
+    hasValidSecondNumber() {
+
+        console.log("hasValidSecondNumber()");
+        let number = this.getValidNumber("Please enter second number.");
+        if (number !== null) {
+            this.secondNumber = number;
+            return true;
+        } else
+            return false;
+    }
+    // Prompts for number using given phrase, and
+    // returns null if provided number is invalid.
+    getValidNumber(phrase) {
+
+        console.log("Start of getValidNumber(), phrase: ", phrase);
+
+        // prompt for the number using the given phrase
+        let strNumber = prompt(phrase);
+    
+        // If the number is empty (blank, null or canceled).
+        if (!strNumber) {
+
+            // If they didn't hit cancel button,
+            // notify them of invalid entry.
+            if (strNumber !== null) 
+                alert("Invalid number.");
+    
+            // Otherwise, just exit.
+            return null;
+        }
+    
+        // if number is not a number
+        let number = parseInt(strNumber);
+        if (isNaN(number)) {
+            alert("'" + strNumber + "' is not a number.");
+            return null;
+        }
+        console.log("End of getValidNumber(), number", number);
+    
+        return number;
+    }    
+    // prompt for and validate operator
+    hasValidOperand() {
 
         // store the operator
         this.operator = prompt("Please enter operator (+, -, *, /).");
         console.log("operator", this.operator);
 
-        // store the second number
-        this.secondPrompt = prompt("Please enter second number.");
-        this.secondNumber = parseInt(this.secondPrompt);
-        console.log("secondNumber", this.secondNumber);
+        // if the operator is empty
+        if (!this.operator) {
+             // if they didn't hit cancel button
+             if (this.operator !== null) 
+                alert("Invalid operator.");
+          
+            return false;
+        }
 
-        if (this.calculator.validArguments(this.firstPrompt, 
-                                            this.operator,
-                                            this.secondPrompt)) 
-            return true;
-        
-        else
-            return false;    
+        // if operator is among the valid list of operators
+        const operators = ["+", "-", "*", "/"];
+        if (!operators.includes(this.operator)) {
+            alert("'" + this.operator + "' is not a valid operator.");
+            return false;
+        }
+
+        return true;
     }
     performCalculations() {
 
@@ -81,24 +121,26 @@ class App{
                 break;
             case "/":
                 this.calculator.divideNumbers(this.firstNumber, this.secondNumber);
-                break;                    
-                    
+                break;                       
             default:
                 alert("Invalid operator \'" + this.operator + "\'.");
                 return null;
         }
         return this.calculator.total;
+
     }
     displayResult() {
          
         let equation = 
-            `${this.firstNumber} ${this.operator} ${this.secondNumber} = ${this.calculator.total}`;
+            `${this.firstNumber} ${this.operator} ${this.secondNumber} = ` +
+            `${this.calculator.total}`;
         console.log(equation);        
         alert(equation); 
     }
 }
 
 let app = new App();
-if (app.promptForInput())
+if (app.hasValidFirstNumber() && app.hasValidOperand() && app.hasValidSecondNumber()) {
     if (app.performCalculations() != null)
         app.displayResult();
+}
